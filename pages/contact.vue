578c884dddb9b0fbc774b5891e14fb4136a0cd85
Ryo Endo <ryoendo@RyoEndos-MacBookPro.local>
2021-10-01 00:06:25 +0900

    title update

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,3 +1,46 @@
 <template>
-  <v-container>Contanct test a</v-container>
+  <v-container>
+    <v-text-field
+      v-model="name"
+      label="お名前"
+      outlined
+      required
+    ></v-text-field>
+    <v-text-field
+      v-model="mail"
+      label="メールアドレス"
+      outlined
+      required
+    ></v-text-field>
+    <v-text-field
+      v-model="form"
+      label="お問い合わせ内容"
+      required
+      outlined
+      height="200"
+    ></v-text-field>
+    <v-container class="text-center"
+      ><v-btn class="ma-1" style="width:120px" to="/">キャンセル</v-btn
+      ><v-btn class="ma-1" color="primary" style="width:120px"
+        >送信</v-btn
+      ></v-container
+    >
+  </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      mail: ""
+    };
+  },
+  created() {
+    var userAgent = window.navigator.userAgent.toLowerCase();
+    console.log(userAgent);
+  }
+};
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,14 @@
 <template>
-  <v-img :src="require('@/assets/img/usamaru.jpg')"></v-img>
+  <v-container>
+    <v-card>
+      <v-img
+        :src="require('@/assets/img/usamaru.jpg')"
+        width="250"
+        style="margin:0px"
+      ></v-img>
+      <v-container class="text-center">
+        <h2>Mochoppyi</h2>
+      </v-container>
+    </v-card>
+  </v-container>
 </template>
